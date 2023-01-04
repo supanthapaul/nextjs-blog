ffaba137b4f0524ac7be2428e63d537eca61d816
Supantha Paul <supapaul56@gmail.com>
2023-01-04 21:49:03 +0530

    Added dynamic routes and polish

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,44 @@
 import Head from 'next/head';
+import Link from 'next/link';
+
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Supantha Paul. I'm a software engineer and indie game developer. You can find me on <a href="https://twitter.com/supanthapaul">Twitter</a>.</p>
-      </section>
-    </Layout>
-  );
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
+
+export default function Home({ allPostsData }) {
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>Hello, I'm Supantha Paul. I'm a software engineer and indie game developer. You can find me on <a href="https://twitter.com/supanthapaul">Twitter</a>.</p>
+			</section>
+
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							<Link href={`/posts/${id}`}>{title}</Link>
+							<br />
+							<small className={utilStyles.lightText}>
+								<Date dateString={date} />
+							</small>
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	);
 }
